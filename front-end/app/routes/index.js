7af552c27125494576f9c9937da66b1a91a775cb
Hans Vertriest <hans.vertriest@gmail.com>
2021-07-14 14:02:50 +0200

    added model and first fetch

--- front-end/app/routes/index.js
+++ front-end/app/routes/index.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default class IndexRoute extends Route {
+  model() {
+    this.store.findAll('road-sign-concept').then((result) => {
+      console.log('result');
+      console.log(result);
+    });
+    return {
+      signs: this.store.findAll('road-sign-concept'),
+    };
+  }
+}

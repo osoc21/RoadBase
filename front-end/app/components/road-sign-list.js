afe8c24a780aba52f7a16ad3ac391deba912ddb8
Hans Vertriest <hans.vertriest@gmail.com>
2021-07-14 16:00:02 +0200

    placing markers on the map

---
+++ front-end/app/components/road-sign-list.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class CounterComponent extends Component {
+  @tracked lng = 4;
+  @tracked lat = 52;
+  @tracked zoom = 12;
+  @tracked location = [52, 4];
+  @action
+  selectItem(sign) {
+    this.args.onSelect(sign);
+  }
+}
--- front-end/app/routes/index.js
+++ front-end/app/routes/index.js
@@ -2,10 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   model() {
-    this.store.findAll('road-sign-concept').then((result) => {
-      console.log('result');
-      console.log(result);
-    });
     return {
       signs: this.store.findAll('road-sign-concept'),
     };

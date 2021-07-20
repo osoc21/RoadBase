88c9e753605cf058cafb46e4ba2615dad0cef3fa
Hans Vertriest <hans.vertriest@gmail.com>
2021-07-20 16:30:14 +0200

    wip filtering sign-list

--- front-end/app/components/road-sign-list.js
+++ front-end/app/components/road-sign-list.js
@@ -1,14 +1,26 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class CounterComponent extends Component {
-  @tracked lng = 4;
-  @tracked lat = 52;
-  @tracked zoom = 12;
-  @tracked location = [52, 4];
+  @tracked selectedSigns = service().query('road-sign-concept', {
+    filter: { 'road-sign-concept-code': 'B9' },
+  });
+
   @action
   selectItem(sign) {
     this.args.onSelect(sign);
+  }
+
+  @action
+  filterSignsOnSearch(event) {
+    console.log(this.selectedSigns);
+    this.selectedSigns = this.args.signs.filter((sign) => {
+      const searchTerm = event.target.value;
+      console.log(sign);
+
+      return sign.roadSignConceptCode.includes(searchTerm);
+    });
   }
 }
--- front-end/app/routes/index.js
+++ front-end/app/routes/index.js
@@ -3,7 +3,9 @@ import Route from '@ember/routing/route';
 export default class IndexRoute extends Route {
   model() {
     return {
-      signs: this.store.findAll('road-sign-concept'),
+      signs: this.store.query('road-sign-concept', {
+        filter: { 'road-sign-concept-code': 'B9' },
+      }),
       instances: this.store.findAll('road-sign-instance'),
     };
   }

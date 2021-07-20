2617f21e79f47442e4c30d8ee4ae6144290a3b71
Bo <bo.robbrecht@student.howest.be>
2021-07-20 14:47:39 +0200

    Able to show dropdown & searchbar for authorities with PowerSelect

--- front-end/app/components/add-sign/add-sign-four.js
+++ front-end/app/components/add-sign/add-sign-four.js
@@ -1,3 +1,10 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class AddSignAddSignFourComponent extends Component {}
+export default class AddSignAddSignFourComponent extends Component {
+  @tracked cities = ['Aalst', 'Antwerpen', 'Aalter'];
+
+  @action
+  foo() {}
+}

f90dfabf5a35d5bc6191ebecc03e359087107417
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2021-07-20 16:57:40 +0200

    fix gitignore

--- front-end/app/components/add-sign/add-sign-container.js
+++ front-end/app/components/add-sign/add-sign-container.js
@@ -1,1 +1,37 @@
-import Component from '@glimmer/component';import { tracked } from '@glimmer/tracking';import { action } from '@ember/object';export default class AddSignContainerComponent extends Component {  @tracked currentStep = 0;  @tracked stepTitles = [    'Borden en hoogte',    'Positie',    'Windrichting',    'Beheer',    'Overzicht',  ];  get currentTitle() {    return this.stepTitles[this.currentStep];  }  @action  incrementCurrentStep() {    if (this.currentStep + 1 < this.stepTitles.length) {      this.currentStep += 1;    }  }  @action  decrementCurrentStep() {    if (this.currentStep - 1 >= 0) {      this.currentStep -= 1;    }  }  @action  setCurrentStep(value) {    this.currentStep = value;  }}
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class AddSignContainerComponent extends Component {
+  @tracked currentStep = 0;
+  @tracked stepTitles = [
+    'Borden en hoogte',
+    'Positie',
+    'Windrichting',
+    'Beheer',
+    'Overzicht',
+  ];
+
+  get currentTitle() {
+    return this.stepTitles[this.currentStep];
+  }
+
+  @action
+  incrementCurrentStep() {
+    if (this.currentStep + 1 < this.stepTitles.length) {
+      this.currentStep += 1;
+    }
+  }
+
+  @action
+  decrementCurrentStep() {
+    if (this.currentStep - 1 >= 0) {
+      this.currentStep -= 1;
+    }
+  }
+
+  @action
+  setCurrentStep(value) {
+    this.currentStep = value;
+  }
+}

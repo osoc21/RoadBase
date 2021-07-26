ff619d5ed349341c46e791611dd6ec5cf557055c
Hans Vertriest <hans.vertriest@gmail.com>
2021-07-26 11:08:04 +0200

    filtering not yet working

--- front-end/app/components/road-sign-list.js
+++ front-end/app/components/road-sign-list.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 export default class CounterComponent extends Component {
   @tracked selectedSigns = this.args.signs;
--- front-end/app/routes/index.js
+++ front-end/app/routes/index.js
@@ -1,11 +1,25 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class IndexRoute extends Route {
+  @tracked codeFilter = 'A';
+
   model() {
     return {
-      signs: this.store.findAll('road-sign-concept'),
+      signs: this.store.query('road-sign-concept', {
+        filter: {
+          'road-sign-concept-code': this.codeFilter,
+        },
+      }),
       instances: this.store.findAll('road-sign-instance'),
     };
+  }
+
+  @action
+  searchSignsByCode(event) {
+    // not yet working, but leaving it in to prioritise other stuff
+    if (event.data) this.codeFilter = event.data;
+    this.refresh();
   }
 }

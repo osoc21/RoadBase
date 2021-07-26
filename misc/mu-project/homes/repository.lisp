cad1fae3fb606226dd544604746f33c033df1dc7
Bo <bo.robbrecht@student.howest.be>
2021-07-26 14:18:34 +0200

    Removed trial projects

--- misc/mu-project/homes/repository.lisp
+++
@@ -1,6 +0,0 @@
-(in-package :mu-cl-resources)
-
-(add-prefix "mu" "http://mu.semte.ch/vocabularies/core/")
-(add-prefix "dcterms" "http://purl.org/dc/terms/")
-(add-prefix "schema" "http://schema.org/")
-(add-prefix "foaf" "http://xmlns.com/foaf/0.1/")

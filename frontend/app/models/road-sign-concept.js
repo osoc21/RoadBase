530d0c3a408d78983d425e61c26237d615bfa782
Hans Vertriest <hans.vertriest@gmail.com>
2021-07-27 16:41:06 +0200

    added markers

--- frontend/app/models/road-sign-concept.js
+++ frontend/app/models/road-sign-concept.js
@@ -1,7 +1,8 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class RoadSignConceptModel extends Model {
   @attr('string') image;
   @attr('string') meaning;
   @attr('string') roadSignConceptCode;
+  @hasMany('road-sign-instance') roadSignInstance;
 }

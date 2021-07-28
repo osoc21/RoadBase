7b6eddbdd7ae7a40859fda7a49fea3063d74196f
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2021-07-28 14:59:09 +0200

    Default API host to http://localhost:8000 to not break local development

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -2,5 +2,6 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import ENV from 'front-end/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  host = "PLACEHOLDER_HOSTNAME_WILL_GET_REPLACED_ON_RUNTIME";
+  // Will get replaced in Docker container on runtime if necessary
+  host = "http://localhost:8000";
 }

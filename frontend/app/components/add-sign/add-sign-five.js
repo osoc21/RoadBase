0fe39feabfa76f5209fcc5f4a725584d77c0f0bb
Hans Vertriest <hans.vertriest@gmail.com>
2021-07-29 09:17:10 +0200

    fix: step 5

--- frontend/app/components/add-sign/add-sign-five.js
+++ frontend/app/components/add-sign/add-sign-five.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
-// import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class AddSignAddSignFiveComponent extends Component {}
+export default class AddSignAddSignFiveComponent extends Component {
+  @service addSign;
+}
